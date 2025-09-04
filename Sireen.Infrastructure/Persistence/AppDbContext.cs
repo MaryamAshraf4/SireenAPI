2346c9e697cd8dad1ffc6df325fce3c85faef6a5
MaryamAshraf4 <maryamashraf386@gmail.com>
2025-09-04 22:10:19 +0300

    chore(infrastructure): add Persistence folder and AppDbContext

---
+++ Sireen.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Sireen.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sireen.Infrastructure.Persistence
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public DbSet<Hotel> Hotels { get; set; } = null!;
+        public DbSet<HotelImage> HotelImages { get; set; } = null!;
+        public DbSet<Room> Rooms { get; set; } = null!;
+        public DbSet<RoomImage> RoomImages { get; set; } = null!;
+        public DbSet<Booking> Bookings { get; set; } = null!;
+        public DbSet<Rating> Ratings { get; set; } = null!;
+        public DbSet<Amenity> Amenities { get; set; } = null!;
+        public DbSet<Payment> Payments { get; set; } = null!;
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
