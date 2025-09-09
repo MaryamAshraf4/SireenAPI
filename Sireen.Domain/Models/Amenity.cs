22dfa812a235171c95e53cae8e3c4169b5cfd7e5
MaryamAshraf4 <maryamashraf386@gmail.com>
2025-09-09 18:22:42 +0300

    refactor: update Amenity model with two new properties

--- Sireen.Domain/Models/Amenity.cs
+++ Sireen.Domain/Models/Amenity.cs
@@ -10,6 +10,8 @@ namespace Sireen.Domain.Models
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsFree { get; set; } = true;
         public ICollection<Room> Rooms { get; set; } = new List<Room>();
     }
 }
