980de4dc532076e49d9dd526259a4820d0e5e05f
MaryamAshraf4 <maryamashraf386@gmail.com>
2025-09-09 18:35:20 +0300

    feat: add Amenity DTOs (Create, Update, Response)

---
+++ Sireen.Application/DTOs/Amenities/CreateAmenityDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sireen.Application.DTOs.Amenities
+{
+    public class CreateAmenityDto
+    {
+        [Required(ErrorMessage = "Amenity name is required.")]
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsFree { get; set; } = true;
+    }
+}
