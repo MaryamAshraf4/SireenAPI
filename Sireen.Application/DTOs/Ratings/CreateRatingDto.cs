86d45cb852870117f3a8d6d173b4def2bb98302d
MaryamAshraf4 <maryamashraf386@gmail.com>
2025-09-12 02:47:13 +0300

    Add CreateRatingDto and RatingDto for rating feature

---
+++ Sireen.Application/DTOs/Ratings/CreateRatingDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sireen.Application.DTOs.Ratings
+{
+    public class CreateRatingDto
+    {
+        [Required(ErrorMessage = "Score is required.")]
+        [Range(1, 5, ErrorMessage = "Score must be between 1 and 5.")]
+        public int Score { get; set; }
+        public string? Comment { get; set; } = string.Empty;
+        public int HotelId { get; set; }
+    }
+}
---
+++ Sireen.Application/DTOs/Ratings/RatingDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sireen.Application.DTOs.Ratings
+{
+    public class RatingDto
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public string Comment { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string ClientFullName { get; set; } = string.Empty;
+        public string ClientNationality { get; set; } = string.Empty;
+    }
+}
