3d136e527810a39b8d9b4be98eb4f49ded132b8f
MaryamAshraf4 <maryamashraf386@gmail.com>
2025-09-22 00:04:32 +0300

    feat(dto): add HotelImage DTO

---
+++ Sireen.API/DTOs/HotelImages/HotelImageUploadDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Sireen.API.DTOs.HotelImages
+{
+    public class HotelImageUploadDto
+    {
+        public int HotelId { get; set; }
+        public IFormFile Image { get; set; } = null!;
+    }
+}
