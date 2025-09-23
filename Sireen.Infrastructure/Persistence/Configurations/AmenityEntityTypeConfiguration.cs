307f6a008c483de4cb3b16e2f17db8e2c54da005
MaryamAshraf4 <maryamashraf386@gmail.com>
2025-09-23 23:26:20 +0300

    Add Fluent API configurations for Amenity and AppUser entities

---
+++ Sireen.Infrastructure/Persistence/Configurations/AmenityEntityTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sireen.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sireen.Infrastructure.Persistence.Configurations
+{
+    public class AmenityEntityTypeConfiguration : IEntityTypeConfiguration<Amenity>
+    {
+        public void Configure(EntityTypeBuilder<Amenity> builder)
+        {
+            builder.Property(a => a.Name).HasMaxLength(100).IsRequired();
+            builder.Property(a => a.Description).HasMaxLength(500);
+        }
+    }
+}
---
+++ Sireen.Infrastructure/Persistence/Configurations/AppUserEntityTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sireen.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sireen.Infrastructure.Persistence.Configurations
+{
+    public class AppUserEntityTypeConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.Property(u => u.FullName).HasMaxLength(200).IsRequired();
+            builder.Property(u => u.IdentityNumber).HasMaxLength(50).IsRequired();
+            builder.Property(u => u.Nationality).HasMaxLength(100).IsRequired();
+            builder.Property(u => u.PhoneNumber).HasMaxLength(20).IsRequired();
+            builder.Property(u => u.Email).IsRequired();
+            builder.HasQueryFilter(u => !u.IsDeleted);
+            builder.Property(u => u.IdentityType).HasConversion<string>().IsRequired();
+        }
+    }
+}
