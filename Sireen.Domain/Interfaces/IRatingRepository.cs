4f07c6fe18be6afb038cca329d0b7270a60d52c0
MaryamAshraf4 <maryamashraf386@gmail.com>
2025-10-06 00:56:47 +0300

    Add IRatingRepository interface and RatingRepository implementation

---
+++ Sireen.Domain/Interfaces/IRatingRepository.cs
@@ -0,0 +1,17 @@
+﻿using Sireen.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sireen.Domain.Interfaces
+{
+    public interface IRatingRepository
+    {
+        Task AddRatingAsync(Rating rating);
+        Task<double> GetAverageRatingByHotelAsync(int hotelId);
+        Task<IEnumerable<Rating>> GetRatingsByHotelAsync(int hotelId);
+        Task<IEnumerable<Rating>> GetRatingsByUserAsync(string userId);
+    }
+}
---
+++ Sireen.Infrastructure/Repositories/RatingRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sireen.Domain.Interfaces;
+using Sireen.Domain.Models;
+using Sireen.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sireen.Infrastructure.Repositories
+{
+    public class RatingRepository : IRatingRepository
+    {
+        private readonly AppDbContext _context;
+        public RatingRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddRatingAsync(Rating rating)
+        {
+            await _context.Ratings.AddAsync(rating);
+        }
+
+        public async Task<double> GetAverageRatingByHotelAsync(int hotelId)
+        {
+            var ratings = await _context.Ratings.Where(r => r.HotelId == hotelId).ToListAsync();
+
+            return ratings.Any() ? ratings.Average(r => r.Score) : 0.0;
+        }
+
+        public async Task<IEnumerable<Rating>> GetRatingsByHotelAsync(int hotelId)
+        {
+            return await _context.Ratings.Include(r => r.User).Where(r => r.HotelId  == hotelId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Rating>> GetRatingsByUserAsync(string userId)
+        {
+            return await _context.Ratings.Include(r => r.Hotel).Where(r => r.UserId == userId) .ToListAsync();
+        }
+    }
+}
