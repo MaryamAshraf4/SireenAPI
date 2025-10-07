617ab4d5ab6d329be711f63d152aa58b6b9c7042
MaryamAshraf4 <maryamashraf386@gmail.com>
2025-10-07 15:43:36 +0300

    Add IAmenityRepository interface and AmenityRepository implementation

---
+++ Sireen.Domain/Interfaces/IAmenityRepository.cs
@@ -0,0 +1,17 @@
+﻿using Sireen.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sireen.Domain.Interfaces
+{
+    public interface IAmenityRepository
+    {  
+        Task AddAsync(Amenity amenity);
+        Task<Amenity?> GetByIdAsync(int id);
+        Task<IEnumerable<Amenity>> GetAllAsync();
+        Task<IEnumerable<Amenity>> GetByIdsAsync(IEnumerable<int> ids);
+    }
+}
---
+++ Sireen.Infrastructure/Repositories/AmenityRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sireen.Domain.Interfaces;
+using Sireen.Domain.Models;
+using Sireen.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sireen.Infrastructure.Repositories
+{
+    public class AmenityRepository : IAmenityRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AmenityRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Amenity amenity)
+        {
+            await _context.Amenities.AddAsync(amenity);
+        }
+
+        public async Task<IEnumerable<Amenity>> GetAllAsync()
+        {
+            return await _context.Amenities.ToListAsync();
+        }
+
+        public async Task<Amenity?> GetByIdAsync(int id)
+        {
+            return await _context.Amenities.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Amenity>> GetByIdsAsync(IEnumerable<int> ids)
+        {
+            return await _context.Amenities.Where(a => ids.Contains(a.Id)).ToListAsync();
+        }
+    }
+}
